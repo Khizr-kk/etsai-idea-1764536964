97e936b3ad707a471220d21c38ccbb8eccc19899
Khizr Anwer <khizranwer40@gmail.com>
2025-12-01 02:41:16 +0530

    Add ml/baseline_model.py

---
+++ ml/baseline_model.py
@@ -0,0 +1,9 @@
+"""
+Baseline ML model for BoomBaam
+
+ML feasibility: yes
+Task: recommendation | classification | regression
+"""
+
+def train_baseline_model(data_path):
+    print("Training baseline model on:", data_path)
